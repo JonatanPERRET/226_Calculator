f35dc220a8e42ba38916ca7515bf205d433b71e2
JonatanPERRET <61775674+JonatanPERRET@users.noreply.github.com>
2021-09-14 17:02:05 +0200

    updated code to MathsRequest

---
+++ MathsLib/MathsRequest.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace MathsLib
+{
+    public class MathsRequest
+    {
+        #region private variables
+        int _op1;
+        int _op2;
+        char _oper;
+        #endregion
+        #region public methods
+        public MathsRequest(int op1, char oper, int op2)
+        {
+            _op1 = op1;
+            _oper = oper;
+            _op2 = op2;
+        }
+
+        public int Op1
+        {
+            get
+            {
+                return _op1;
+            }
+        }
+        public char Oper
+        {
+            get
+            {
+                return _oper;
+            }
+        }
+
+        public int Op2
+        {
+            get
+            {
+                return _op2;
+            }
+        }
+        
+        #endregion
+    }
+}
